e36f6c560e3efa7ccf99b98543e90960ef42a98e
Nabilfadilah <mohammadfadilah00@gmail.com>
2022-10-25 18:22:53 +0700

    Menyelesaikan video 8. Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,9 @@
 @section('container')
 
     <h2 class="mb-5">{{ $post->title }}</h2>
+
+    <p>By. Mohammad Nabil Fadilah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {{-- <h5>{{ $post["author"] }}</h5> --}}
     {!! $post->body !!}
 
