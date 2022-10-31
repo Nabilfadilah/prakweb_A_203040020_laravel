3027d4f58a70ef75c436df3b98ce51b38e8ef182
Nabilfadilah <mohammadfadilah00@gmail.com>
2022-10-31 17:17:18 +0700

    Menyelesaikan video 13. Searching & Pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,23 @@
 
 @section('container')
 
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row mb-3">
+    <div class="col-mb-6">
+        <form action="/blog">
+
+            @if (request('category')) 
+                <input type="hidden" name="category" value="{{ request('category') }}">
+            @endif
+
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+                <button class="btn btn-danger" type="submit">Search</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 @if ($posts->count())
     {{-- framework Bootstrap --}}
@@ -15,8 +31,8 @@
 
         <p>
             <small class="text-muted">
-                By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}        
-                </a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+                By. <a href="/blog?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}        
+                </a> in <a href="/blog?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
             </small>
         </p>
             <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -26,9 +42,7 @@
     </div>
 </div>
     
-@else
-    <p class="text-center fs-4">No Post found.</p>
-@endif
+
 
 {{-- card --}}
 <div class="container">
@@ -37,13 +51,13 @@
 
         <div class="col-md-4 mb-3">
             <div class="card">
-                <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+                <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/blog?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                 <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <p>
                         <small class="text-muted">
-                            By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}        
+                            By. <a href="/blog?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}        
                             </a> {{ $post->created_at->diffForHumans() }}
                         </small>
                     </p>
@@ -55,6 +69,14 @@
 
         @endforeach
     </div>
+</div>
+
+@else
+    <p class="text-center fs-4">No Post found.</p>
+@endif
+
+<div class="d-flex justify-content-center">
+    {{ $posts->links() }}
 </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        'active' => 'home'
     ]);
 });
 
+// kalau pak sandhika kutipnya ''
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        'active' => 'about',
         "name" => "M Nabil Fadilah",
         "email" => "nabIlFadilah8@gmail.com",
         "image" => "nabil.png"
@@ -46,18 +49,19 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post by Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/categories/{category:slug}', function (Category $category) {
+//     return view('posts', [
+//         'title' => "Post by Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
 
 
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('Posts', [
-        'title' => "Post by Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
+// Route::get('/authors/{author:username}', function (User $author) {
+//     return view('Posts', [
+//         'title' => "Post by Author : $author->name",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category', 'author'),
+//     ]);
+// });
